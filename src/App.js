ae13f7347d499fe2117e9b869bfc357aa0e867b7
YanPanMichael <yanpancsu@icloud.com>
2019-07-02 01:07:15 +0800

    implement with react router and add login component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
-import { Layout } from "antd";
-import CollectionsPage from "./components/CollectionsPage";
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./routes/Login/index";
+import Index from "./routes/Index/index";
 import "./App.css";
 
-const { Header, Footer, Content } = Layout;
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Header className="portal-header">
-          <h2>Citi</h2>
-        </Header>
-        <Content className="portal-content">
-          <CollectionsPage />
-        </Content>
-        <Footer className="portal-footer">
-          <p>Made with in China.</p>
-          <p>2019 CSTC Co. All rights reserved.</p>
-        </Footer>
-      </Layout>
-    </div>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <PrivateRoute path="/" component={Index} />
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { LocaleProvider } from "antd";
+import zh_CN from "antd/lib/locale-provider/zh_CN";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <LocaleProvider locale={zh_CN}>
+      <App />
+    </LocaleProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
